e651b3d09b44a404e401f237c1f4c683967cbf52
Khalil <100420997+khaliiljama@users.noreply.github.com>
2022-03-08 05:25:06 +0200

    Update randomhaku.js

--- randomhaku.js
+++ randomhaku.js
@@ -1,15 +1,18 @@
 'use strict';
-let Randomizer = document.querySelector('#random');
-Randomizer.addEventListener('click', function(){
-  let nappirandom = document.getElementById('hakunappi')
-  let Main1 = document.querySelector('main');
-  let osoite = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-  let Keho1 = document.querySelector('body');
-
-  fetch(osoite).then(res => res.json()).then(data => {
+let Randomizer = document.querySelector('#random');       // Valitsee Random napin HTML tiedostosta
+Randomizer.addEventListener('click', function(){      // Lisätään kuuntelijafunktio, joka suorittaa sisälle luotu funktiot, kun sitä painaa
+  let nappirandom = document.getElementById('hakunappi')   
+  let Main1 = document.querySelector('main'); // Luodaan Mainille arvo
+  let osoite = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'; // random-haun api osoite
+  let Keho1 = document.querySelector('body'); // LIsätään bodylle arvo
+                                            
+  // API fetch haku alkaa tästä
+  fetch(osoite).then(res => res.json()).then(data => { 
 
     let data2 = data.drinks
-    console.log(data)
+    console.log(data)       // Konsoliin lisätään tiedot
+    
+    // Luodaan funktio, jossa lisätään konsoliin saadut tiedot sivustoon
     function reseptit (cv, index, data2) {
 
       let Figu = document.createElement('figure');
@@ -21,12 +24,12 @@ Randomizer.addEventListener('click', function(){
       const Aineet = document.createElement('ul')
       let Brk = document.createElement('br')
       let Head = document.createElement('h3')
-      Head.innerText = 'Type'
+      Head.innerText = 'Type' // Innertextillä lisätään tiedot HTML tageihin, kuten headeriin jne.
 
       let Kategoria = document.createElement('p')
-      Kategoria.innerText = data2[index].strCategory
+      Kategoria.innerText = data2[index].strCategory // Innertextillä lisätään tiedot HTML tageihin, kuten headeriin jne.
 
-      Header3.innerText = 'Ingredients '
+      Header3.innerText = 'Ingredients ' 
 
       Ohjeet.innerText = 'Instructions'
 
@@ -35,9 +38,9 @@ Randomizer.addEventListener('click', function(){
       Annos.innerText = 'Serving Method'
 
       let Metodi = document.createElement('p')
-      Metodi.innerText = 'Serve in a ' + data2[index].strGlass + '.'
+      Metodi.innerText = 'Serve in a ' + data2[index].strGlass + '.' // Innertextillä lisätään tiedot HTML tageihin, kuten headeriin jne.
 
-      const Ainesosat = [];
+      const Ainesosat = []; // Tässä luodaan taulukot ainesosille
       Ainesosat[0] = data2[index].strIngredient1
       Ainesosat[1] = data2[index].strIngredient2
       Ainesosat[2] = data2[index].strIngredient3
@@ -54,7 +57,7 @@ Randomizer.addEventListener('click', function(){
       Ainesosat[13] = data2[index].strIngredient14
       Ainesosat[14] = data2[index].strIngredient15
 
-      const Maara = [];
+      const Maara = []; // Tässä toinen taulukko ainesosien määrille
       Maara[0] = data2[index].strMeasure1;
       Maara[1] = data2[index].strMeasure2;
       Maara[2] = data2[index].strMeasure3;
@@ -71,6 +74,7 @@ Randomizer.addEventListener('click', function(){
       Maara[13] = data2[index].strMeasure14;
       Maara[14] = data2[index].strMeasure15;
 
+      // Tämä funktio vaihtaa ainesosan tai määrän sanoilla "Your choice", jos sen arvo on "null" tai "undefined".
       function del (cv, index, measure) {
         if (measure[index] == null) {
           measure[index] = 'Your choice';
@@ -82,6 +86,7 @@ Randomizer.addEventListener('click', function(){
       Maara.forEach(del)
 
 
+      // For looppi lisää arvot listaan.
       for (let i = 0; i < Ainesosat.length; i++) {
         let li = document.createElement('li')
         li.innerText = Ainesosat[i] + ' : ' + Maara[i]
@@ -94,6 +99,7 @@ Randomizer.addEventListener('click', function(){
         }
       }
 
+      // Nämä ovat kielinapit, eli luodaan button elementti ja lisätään innertextiin kieli 
       let NappiEng = document.createElement('button')
       NappiEng.innerText = 'ENG'
 
@@ -109,7 +115,9 @@ Randomizer.addEventListener('click', function(){
       let NappiEs = document.createElement('button')
       NappiEs.innerText = 'ES'
 
-
+      
+      // Tässä lisätään kuuntelijat kielinapeille ja lisätään aiemmin luotuun paragraph arvoon eri kielet, jos niitä ei löydy niin siihen tulee "Not available".
+      
       NappiDeu.addEventListener('click', function() {
         Paragraph.innerText = data2[index].strInstructionsDE
         if ( data2[index].strInstructionsDE == null)
@@ -144,14 +152,15 @@ Randomizer.addEventListener('click', function(){
         }
       })
 
-      DrinkImage.src = data2[index].strDrinkThumb
+      DrinkImage.src = data2[index].strDrinkThumb // Juomien kuvat lisätään innertextillä
 
 
 
-      Header2.innerText = data2[index].strDrink
+      Header2.innerText = data2[index].strDrink // Juoman nimi lisätään innertextillä
 
-      Paragraph.innerText = data2[index].strInstructions
+      Paragraph.innerText = data2[index].strInstructions // Juomien ohjeet lisätään innertextillä
 
+      // Sama kieli nappi, kuin aikaisemmin nyt vaan englanniksi
       NappiEng.addEventListener('click', function() {
         Paragraph.innerText = data2[index].strInstructions
         if ( data2[index].strInstructions == null)
@@ -161,6 +170,7 @@ Randomizer.addEventListener('click', function(){
         }
       })
 
+      // Tässä kohtaa appendChildiä käyttäen lisätään kaikki arvot figureen
       Figu.appendChild(Header2)
       Figu.appendChild(DrinkImage)
       Figu.appendChild(Ohjeet)
@@ -180,9 +190,11 @@ Randomizer.addEventListener('click', function(){
       Main1.appendChild(Figu)
       Keho1.appendChild(Main1)
 
+      //Tämä poistaa vanhan figure arvon napin painalluksella
       Randomizer.addEventListener('click', function() {
         Figu.remove()
       })
+      // Hakunappi poistaa myös vanhan arvon
       nappirandom.addEventListener('click', function (){
         Figu.remove()
       })
